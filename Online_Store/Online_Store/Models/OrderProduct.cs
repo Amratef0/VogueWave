917ad1bdb5103d48fc9d57e11b2c95702b4b1ed8
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-05-09 23:38:32 +0300

    Update OrderProduct.cs
    updated specific line in OrderProductModel

--- Online_Store/Online_Store/Models/OrderProduct.cs
+++ Online_Store/Online_Store/Models/OrderProduct.cs
@@ -8,7 +8,7 @@
         public int? ProductId { get; set; }
         public Product? Product { get; set; }
 
-        public int? Quantity { get; set; } // كمية المنتج في الطلب
+        public int? Quantity { get; set; } 
     }
 
 }
