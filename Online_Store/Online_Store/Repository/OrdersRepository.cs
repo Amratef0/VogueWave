a2eaa4ac5f1ef9a8bba02a7f4135694c82834400
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-05-09 23:35:37 +0300

    Update OrdersRepository.cs
    updated specific line in OrdersRepo

--- Online_Store/Online_Store/Repository/OrdersRepository.cs
+++ Online_Store/Online_Store/Repository/OrdersRepository.cs
@@ -19,38 +19,32 @@ namespace Online_Store.Repository
             _context.Add(order);
         }
 
-        // تحديث طلب
         public void UpdateOrder(Order order)
         {
             _context.Update(order);
         }
 
-        // حذف طلب
         public void DeleteOrder(Order order)
         {
             _context.Remove(order);
         }
 
-        // حذف طلب بواسطة الـ ID
         public void DeleteOrderById(int id)
         {
             Order order = GetOrderById(id);
             _context.Remove(order);
         }
 
-        // الحصول على جميع الطلبات
         public List<Order> GetAllOrders()
         {
             return _context.Orders.ToList();
         }
 
-        // الحصول على طلب بواسطة الـ ID
         public Order GetOrderById(int id)
         {
             return _context.Orders.FirstOrDefault(o => o.Id == id);
         }
 
-        // حفظ التغييرات في الطلبات
         public void SaveOrderChanges()
         {
             _context.SaveChanges();
@@ -60,7 +54,7 @@ namespace Online_Store.Repository
             return _context.Orders
                 .Include(o => o.OrderProducts)
                 .ThenInclude(op => op.Product)
-                .FirstOrDefault(o => o.OrderNumber == trackingNumber); // البحث باستخدام رقم التتبع
+                .FirstOrDefault(o => o.OrderNumber == trackingNumber); 
         }
 
        
