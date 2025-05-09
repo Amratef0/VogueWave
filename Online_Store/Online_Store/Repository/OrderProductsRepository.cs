8095f2dff54b2ea790afb7445382f3776d98a7b8
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-05-09 23:35:05 +0300

    Update OrderProductsRepository.cs
    updated specific line in OrderProductsRepo

--- Online_Store/Online_Store/Repository/OrderProductsRepository.cs
+++ Online_Store/Online_Store/Repository/OrderProductsRepository.cs
@@ -12,38 +12,32 @@ namespace Online_Store.Repository
             _context = context;
         }
 
-        // إضافة OrderProduct جديد
         public void AddOrderProduct(OrderProduct orderProduct)
         {
             _context.Add(orderProduct);
         }
 
-        // تحديث OrderProduct
         public void UpdateOrderProduct(OrderProduct orderProduct)
         {
             _context.Update(orderProduct);
         }
 
-        // حذف OrderProduct
         public void DeleteOrderProduct(OrderProduct orderProduct)
         {
             _context.Remove(orderProduct);
         }
 
-        // حذف OrderProduct بواسطة الـ ID
         public void DeleteOrderProductById(int id)
         {
             OrderProduct orderProduct = GetOrderProductByOrderAndProductId(id , id);
             _context.Remove(orderProduct);
         }
 
-        // الحصول على جميع OrderProducts
         public List<OrderProduct> GetAllOrderProducts()
         {
             return _context.OrderProducts.ToList();
         }
 
-        // الحصول على OrderProduct بواسطة الـ ID
         public OrderProduct GetOrderProductByOrderAndProductId(int orderId, int productId)
         {
             return _context.OrderProducts
@@ -51,7 +45,6 @@ namespace Online_Store.Repository
         }
 
 
-        // حفظ التغييرات في OrderProducts
         public void SaveOrderProductChanges()
         {
             _context.SaveChanges();
