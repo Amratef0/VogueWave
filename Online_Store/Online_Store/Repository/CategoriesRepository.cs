37341a256bbe0a21426e43ce108697dcfbff1602
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-05-09 23:34:27 +0300

    Update CategoriesRepository.cs
    updated specific line in CategoriesRepo

--- Online_Store/Online_Store/Repository/CategoriesRepository.cs
+++ Online_Store/Online_Store/Repository/CategoriesRepository.cs
@@ -16,38 +16,32 @@ namespace Online_Store.Repository
             _context.Add(category);
         }
 
-        // تحديث تصنيف
         public void UpdateCategory(Category category)
         {
             _context.Update(category);
         }
 
-        // حذف تصنيف
         public void DeleteCategory(Category category)
         {
             _context.Remove(category);
         }
 
-        // حذف تصنيف بواسطة الـ ID
         public void DeleteCategoryById(int id)
         {
             Category category = GetCategoryById(id);
             _context.Remove(category);
         }
 
-        // الحصول على جميع التصنيفات
         public List<Category> GetAllCategories()
         {
             return _context.Categories.ToList();
         }
 
-        // الحصول على تصنيف بواسطة الـ ID
         public Category GetCategoryById(int id)
         {
             return _context.Categories.FirstOrDefault(c => c.Id == id);
         }
 
-        // حفظ التغييرات في التصنيفات
         public void SaveCategoryChanges()
         {
             _context.SaveChanges();
