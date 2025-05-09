f36bdde5091563603d25e4788b4a9e9bdc18c96c
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-05-09 23:36:13 +0300

    Update ProductImagesRepository.cs
    updated specific line in ProductImagesRepo

--- Online_Store/Online_Store/Repository/ProductImagesRepository.cs
+++ Online_Store/Online_Store/Repository/ProductImagesRepository.cs
@@ -13,44 +13,37 @@ namespace Online_Store.Repository
             _context = context;
         }
 
-        // إضافة ProductImage جديد
         public void AddProductImage(ProductImage productImage)
         {
             _context.Add(productImage);
         }
 
-        // تحديث ProductImage
         public void UpdateProductImage(ProductImage productImage)
         {
             _context.Update(productImage);
         }
 
-        // حذف ProductImage
         public void DeleteProductImage(ProductImage productImage)
         {
             _context.Remove(productImage);
         }
 
-        // حذف ProductImage بواسطة الـ ID
         public void DeleteProductImageById(int id)
         {
             ProductImage productImage = GetProductImageById(id);
             _context.Remove(productImage);
         }
 
-        // الحصول على جميع ProductImages
         public List<ProductImage> GetAllProductImages()
         {
             return _context.ProductImages.ToList();
         }
 
-        // الحصول على ProductImage بواسطة الـ ID
         public ProductImage GetProductImageById(int id)
         {
             return _context.ProductImages.FirstOrDefault(pi => pi.Id == id);
         }
 
-        // حفظ التغييرات في ProductImages
         public void SaveProductImageChanges()
         {
             _context.SaveChanges();
@@ -58,12 +51,12 @@ namespace Online_Store.Repository
         public void DeleteImagesByProductId(int productId)
         {
             var productFromDb = _context.Products
-                .Include(p => p.ProductImages)  // التأكد من تحميل الصور المرتبطة بالمنتج
+                .Include(p => p.ProductImages) 
                 .FirstOrDefault(p => p.Id == productId);
 
             if (productFromDb != null && productFromDb.ProductImages != null && productFromDb.ProductImages.Any())
             {
-                _context.ProductImages.RemoveRange(productFromDb.ProductImages);  // حذف الصور
+                _context.ProductImages.RemoveRange(productFromDb.ProductImages);  
             }
         }
     }
