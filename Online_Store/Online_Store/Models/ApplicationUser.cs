22f483a5413b6d05f0157ee0cb141155b5069b1b
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-05-10 22:26:41 +0300

    Update ApplicationUser.cs
    Update line in ApplicationUser Model

--- Online_Store/Online_Store/Models/ApplicationUser.cs
+++ Online_Store/Online_Store/Models/ApplicationUser.cs
@@ -4,7 +4,7 @@ namespace Online_Store.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public ICollection<Order> Orders { get; set; } = new List<Order>(); // هذا يعني أن المستخدم يمكنه أن يكون له العديد من الطلبات
+        public ICollection<Order> Orders { get; set; } = new List<Order>(); 
 
         public string? Address { get; set; }
     }
